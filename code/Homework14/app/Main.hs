151dbffbdc9d31df5f45a9ce457ef0054a6250ec
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-08-15 12:42:54 +0200

    Adding homework for lesson 14.

---
+++ code/Homework14/app/Main.hs
@@ -0,0 +1,14 @@
+
+-- Write a program that reads the file Data.txt and checks following properties:
+-- the name parameter contains at least two words 
+-- the email parameter contains an @ sign and the domain name contains a dot
+-- the password parameter is at least six characters long and contains at least one number and a non-number character
+
+-- If any of the checks do not pass return an appropriate error message.
+-- Use the concept of Railway oriented programming as shown in the lesson.
+-- HINT: The library split is added to the cabal file as dependency. 
+
+-- The main function of the programm
+main :: IO ()
+main = do
+    print "Solve the problem."
