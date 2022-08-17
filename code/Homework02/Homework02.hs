03a803bd266de0de5355e0af15c7131eeb21e350
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-08-17 15:16:00 +0200

    Adding homework for lesson 2.

---
+++ code/Homework02/Homework02.hs
@@ -0,0 +1,24 @@
+
+-- Question 1
+-- Are really all variables in Haskell immutable? Try googling for the answer.
+
+
+-- Question 2
+-- Why should we define type signatures of functions? How can they help you? How can they help others?
+
+
+-- Question 3
+-- Why should you define type signatures for variables? How can they help you?
+
+
+-- Question 4
+-- Are there any functions in Haskell that let you transform one type to the other? Try googling for the answer.
+
+
+-- Question 5
+-- How would you write the prod function from our lesson so that it works for Int and Double? Does the code compile?
+
+
+-- Question 6
+-- Can you also define in Haskell list of lists? Did we showed any example of that? How would you access the inner
+-- most elements?
