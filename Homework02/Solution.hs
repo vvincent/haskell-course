c3ee5cb625acb7df3a9a13be5d487a2e81e6c93e
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-08-18 09:51:00 +0200

    Updating solution for lesson 2.

--- Homework02/Solution.hs
+++ Homework02/Solution.hs
@@ -1,5 +1,23 @@
 
 -- Question 1
+-- Add the type signatures for the functions below and then remove the comments and try to compile.
+
+f1 :: Double -> Double -> Double -> Double
+f1 x y z = x ** (y/z)
+
+f2 :: Double -> Double -> Double -> Double
+f2 x y z = sqrt (x/y - z)
+
+f3 :: Char -> Double -> String -> String
+f3 x y z = x:((show y) ++ z)
+
+f4 :: Double -> Double -> Bool -> [Bool]
+f4 x y z = [x > y] ++ [z]
+
+f5 :: String -> String -> String -> Bool
+f5 x y z = x == (y ++ z)
+
+-- Question 2
 -- Are really all variables in Haskell immutable? Try googling for the answer.
 
 -- Not all variables are immutable. An example is the data type STUArray which stands for stateful unboxed array. 
@@ -7,21 +25,21 @@
 -- as a normal unboxed array which is immutable. So there are still some restrictions in Haskell what you can do
 -- with mutable data structures.  
 
--- Question 2
+-- Question 3
 -- Why should we define type signatures of functions? How can they help you? How can they help others?
 
 -- Type signatures place restrictions on your data and can help you to catch errors you make in your code. Also
 -- you can develop your programs by first defining the type signatures of functions which sets the data transformation
 -- flow. And after that you just code the details. They can also help other developers to better understand your code.
 
--- Question 3
+-- Question 4
 -- Why should you define type signatures for variables? How can they help you?
 
 -- In Haskell we ussually declare variables inside functions. We mostly do no use type signatures for them. But in the
 -- begining when you are getting errors it is helpful to add the signatures to all of your variables because the error
 -- from the compiler can become more understandable if you do this.
 
--- Question 4
+-- Question 5
 -- Are there any functions in Haskell that let you transform one type to the other? Try googling for the answer.
 
 -- The function show and read transform a string to a numeric type and vice versa:
@@ -37,7 +55,7 @@ v3 = read v2 :: Double -- another way of adding type signatures to variables
 v4 = 3.1415 :: Double
 v5 = round v4 :: Int
 
--- Question 5
+-- Question 6
 -- How would you write the prod function from our lesson so that it works for Int and Double? Does the code compile?
 
 -- You can write it as following. But you need to use a type class constraint in the type signature of your function.
@@ -47,7 +65,7 @@ v5 = round v4 :: Int
 prod :: Num a => a -> a -> a
 prod x y = x * y
 
--- Question 6
+-- Question 7
 -- Can you also define in Haskell list of lists? Did we showed any example of that? How would you access the inner
 -- most elements?
 
