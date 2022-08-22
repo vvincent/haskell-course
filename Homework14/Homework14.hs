2a6de83b37d4229d691887b79f803a8c2cdf791f
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-08-22 15:15:52 +0200

    Updating solution for homework 14 to a cabal project structure.

---
+++ Homework14/Homework14.hs
@@ -0,0 +1,9 @@
+
+-- Write a program that reads the file Data.txt and checks following properties:
+-- the name parameter contains at least two words 
+-- the email parameter contains an @ sign and the domain name contains a dot
+-- the password parameter is at least six characters long and contains at least one number and a non-number character
+
+-- If any of the checks do not pass return an appropriate error message.
+-- Use the concept of Railway oriented programming as shown in the lesson.
+-- HINT: The library split is added to the cabal file as dependency. 
