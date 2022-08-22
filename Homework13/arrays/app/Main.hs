a171a3e65c7b4dad216ab5a43a45fe3815318ac7
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-08-22 17:59:02 +0200

    Updating solution for homework 13. Gives a compile error. Needs fix.

---
+++ Homework13/arrays/app/Main.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import Data.Array.Unboxed (UArray)
+import Data.Array (Array, bounds, array, (!))
+import Data.Array.ST (writeArray, MArray(newArray), runSTUArray)
+import Control.Monad (forM_)
+
+factorial :: Int -> [Int]
+factorial n = map makeFactorial [1..n]
+  where makeFactorial x = product [1..x]
+
+factorialArray :: Int -> Array Int Int
+factorialArray n = array (1,n) $ zip [1 .. n] $ factorial n
+
+updateArray :: Array Int Int -> UArray Int Int
+updateArray myArray = runSTUArray $ do
+  let arrayLength = snd $ bounds myArray
+  updatedArray <- newArray (1,arrayLength) 0
+  forM_ [1..arrayLength] $ \ind -> do
+        writeArray updatedArray ind $ myArray ! (arrayLength + 1 - ind)
+  return updatedArray
+
+main :: IO ()
+main = do
+  n <- getLine
+  print $ factorialArray $ read n
+  print $ updateArray $ factorialArray $ read n
