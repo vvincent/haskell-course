43d847fd6ca976d6120750afe204431d0cfd4fc6
perturbing <thomas.vellekoop@iohk.io>
2022-08-22 10:08:16 +0200

    change layout of homework01

--- code/Homework01/Homework01.hs
+++ code/Homework01/Homework01.hs
@@ -1,10 +1,16 @@
 
--- Write a function that takes a value and multiplies it by 3. Test that it works using GHCi.
+-- Question 1
+-- Write a multiline comment below.
 
--- Write a function that calculates the area of a circle. Test that it works using GHCi.
+-- Question 2
+-- Define a function that takes a value and multiplies it by 3.
 
--- Write a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
--- Test that it works using GHCi.
+-- Question 3
+-- Define a function that calculates the area of a circle.
 
--- Write a function that checks if the volume of a cylinder is greater than or equal to 42. Test that it works using GHCi.
+-- Question 4
+-- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
+
+-- Question 5
+-- Define a function that checks if the volume of a cylinder is greater than or equal to 42.
 
