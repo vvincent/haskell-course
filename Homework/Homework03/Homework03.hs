938c068bd16dbb5f26f4a175439326c53f6c8a5e
perturbing <thomas.vellekoop@iohk.io>
2022-08-29 10:47:28 +0200

    Fit naming on lesson 3a -> 3

---
+++ Homework/Homework03/Homework03.hs
@@ -0,0 +1,12 @@
+
+-- Question 1
+-- Write a function that takes in two numbers and returns their quotient such that it is not grater then 1.
+-- Return the number as a string and in case that the divisor is 0 return a message why the division is not
+-- possible. To implement this function use both guards and if-else-then statements. 
+
+
+-- Question 2
+-- Write a function that takes in two numbers and calculates the sum of squares for the product and quotient
+-- of those numbers. Write the function such that you use a where statement inside a let-in stamenet and a
+-- let-in stamenet inside a where statement. 
+
