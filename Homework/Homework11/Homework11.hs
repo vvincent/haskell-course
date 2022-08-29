3969592c7587c5c766ba8b5b51867701809a7273
perturbing <thomas.vellekoop@iohk.io>
2022-08-29 10:36:48 +0200

    add homework 11 (basic IO)

---
+++ Homework/Homework11/Homework11.hs
@@ -0,0 +1,20 @@
+import System.Directory (createDirectory, removeDirectory, listDirectory)
+import Data.Time.Clock
+
+-- Question 1
+-- Define an IO action that print the string "Hello World".
+
+
+-- Question 2
+-- Define an IO action that has as input a name from StdIn, and returns "Hello {NAME HERE}".
+
+
+-- Question 3
+-- Define an IO action that count the amount of files in the current directory.
+
+
+-- Question 4
+-- Define an IO action that calculates the time an other IO action takes. HINT: use getCurrentTime and diffUTCTime
+-- from the module Data.Time.Clock
+
+
