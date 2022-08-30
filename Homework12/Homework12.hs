36cde119134c181cb70b08ecec4372d4162db744
Thomas Vellekoop <107037423+perturbing@users.noreply.github.com>
2022-08-30 14:21:57 +0200

    add space

--- Homework12/Homework12.hs
+++ Homework12/Homework12.hs
@@ -33,4 +33,4 @@ integerToByteString :: Integer -> BS.ByteString
 integerToByteString n = BS.pack $ go (abs n)
                         where
                             go 0 = []
-                            go m =go (m `div` 256) ++ [(toEnum . fromIntegral)(m `mod` 256)] 
+                            go m = go (m `div` 256) ++ [(toEnum . fromIntegral)(m `mod` 256)] 
