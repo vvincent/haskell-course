25a873ac06c9b18f71e428d11afb7043eb6a0b03
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-08-31 17:03:56 +0200

    Adding homework for lesson 6.

---
+++ Homework/Homework06/Homework06.hs
@@ -0,0 +1,17 @@
+
+
+-- Question 1
+-- Write a function that takes in an integer n and returns a list of the first n
+-- numbers of the fibonacci list (https://en.wikipedia.org/wiki/Fibonacci_number). 
+
+
+-- Question 2
+-- Write a function that takes in an integer n, calculates the factorial n! and 
+-- returns a string in the form of 1*2* ... *n = n! where n! is the actual result. 
+
+
+-- Question 3
+-- Write a function that takes a number n of type Int and returns a list of prime
+-- numbers that are not greater then the number n. You can use the function "rem"
+-- that gives you the remainder for division.
+
