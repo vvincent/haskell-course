45bafde1d9241441c50d2b5059c17537cfd18334
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-09-01 12:50:29 +0200

    Adding homework for lesson 7.

---
+++ Homework/Homework07/Homework07.hs
@@ -0,0 +1,17 @@
+
+
+-- Question 1
+-- Write your own version of the functions map, sum and filter. Use pattern matching. 
+
+
+-- Question 2
+-- Write your own version of the functions foldl and foldr. 
+
+
+-- Question 3
+-- Write your own version of functions zip and zipWith. Use pattern matching. 
+
+
+-- Question 4
+-- Write your own version of scanl and scanr functions. You can use pattern matching.
+
