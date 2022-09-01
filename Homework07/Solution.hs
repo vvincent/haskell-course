4f75ce33e316c3d755ce45f36a583f3e4766d91a
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-09-01 12:31:06 +0200

    Adding solution for homework 7.

---
+++ Homework07/Solution.hs
@@ -0,0 +1,57 @@
+
+
+-- Question 1
+-- Write your own version of the functions map, sum and filter. Use pattern matching. 
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f [] = []
+myMap f (x:xs) = f x : myMap f xs
+
+mySum :: Num a => [a] -> a
+mySum [x] = x
+mySum (x:xs) = x + (mySum xs)
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f [] = []
+myFilter f (x:xs) = if f x then x : myFilter f xs
+                    else myFilter f xs
+
+-- Question 2
+-- Write your own version of the functions foldl and foldr. 
+
+myFoldL :: (a -> a -> a) -> a -> [a] -> a 
+myFoldL f start [] = start
+myFoldL f start xs = myFoldL f newStart $ tail xs
+  where newStart = f start $ head xs
+
+myFoldR :: (a -> a -> a) -> a -> [a] -> a
+myFoldR f start [x] = f x start 
+myFoldR f start xs = myFoldR f (f (last xs) start) $ init xs
+
+-- Question 3
+-- Write your own version of functions zip and zipWith. Use pattern matching. 
+
+myZip :: [a] -> [b] -> [(a,b)]
+myZip [] _ = []
+myZip _ [] = []
+myZip [x] [y] = [(x,y)]
+myZip (x:xs) (y:ys) = (x,y) : myZip xs ys
+
+myZipWith :: (a -> a -> a) -> [a] -> [a] -> [a]
+myZipWith f [] _ = []
+myZipWith f _ [] = []
+myZipWith f [x] [y] = [f x y]
+myZipWith f (x:xs) (y:ys) = f x y : myZipWith f xs ys
+
+-- Question 4
+-- Write your own version of scnal and scanr functions. You can use pattern matching.
+
+myScanL :: (a -> a -> a) -> a -> [a] -> [a]
+myScanL f x [] = [x]
+myScanL f x [y] = [f x y]
+myScanL f x (y:ys) = x : myScanL f (f x y) (tail ys)
+
+myScanR :: (a -> a -> a) -> a -> [a] -> [a]
+myScanR f x ys = reverse $ myScanR' f x ys
+  where myScanR' f x [] = [x]
+        myScanR' f x ys = x : myScanR' f (f (last ys) x) (init ys)
