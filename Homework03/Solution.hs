97042b0c535e66f39c4a6c581febad527d9d7108
Robertino <48034748+rober-m@users.noreply.github.com>
2022-09-18 22:03:43 +0100

    Added more questions and corrected previous ones

--- Homework03/Solution.hs
+++ Homework03/Solution.hs
@@ -1,8 +1,39 @@
-
 -- Question 1
--- Write a function that takes in two numbers and returns their quotient such that it is not grater then 1.
--- Return the number as a string and in case that the divisor is 0 return a message why the division is not
--- possible. To implement this function use both guards and if-else-then statements. 
+-- Write a function that checks if the monthly consumption of an electrical device is bigger, equal, or smaller than the maximum allowed and
+-- returns a message accordingly. 
+-- The function has to take the hourly consumption of an electrical device, the hours of daily use, and the maximum monthly consumption allowed.
+-- (Monthly usage = consumption (kW) * hours of daily use (h) * 30 days).
+
+checkConsumption consumption time maxC
+    | monthlyC > maxC = "Penguins are disappointed in you!"  
+    | monthlyC == maxC = "Don't push it!"
+    | otherwise = "You're good!"
+    where
+        monthlyC = consumption * time * 30
+
+-- Question 2
+-- Prelude:
+-- We use the function `show :: a -> String` to transform any type into a String.
+-- So `show 3` will produce `"3"` and `show (3 > 2)` will produce `"True"`.
+
+-- In the previous function, return the excess/savings of consumption as part of the message.
+
+checkConsumption consumption time maxC
+    | monthlyC > maxC = "Penguins are disappointed in you. You're wasting: " ++ show (monthlyC - maxC) ++ "kWh per month!"
+    | monthlyC == maxC = "Don't push it!"
+    | otherwise = "You're saving: " ++ show (maxC - monthlyC) ++ "kWh per month! Good job!!"
+    where
+        monthlyC = consumption * time * 30
+
+-- Question 3
+-- Write a function that showcases the advantages of using let expressions to split a big expression into smaller ones.
+-- Then, share it with other students in Canvas.
+
+
+-- Question 4
+-- Write a function that takes in two numbers and returns their quotient such that it is not greater than 1.
+-- Return the number as a string, and in case the divisor is 0, return a message why the division is not
+-- possible. To implement this function using both guards and if-then-else statements.  
 
 guardsAndIf :: Double -> Double -> String
 guardsAndIf a b
@@ -10,10 +41,11 @@ guardsAndIf a b
   | a < b = if b /= 0 then show (b/a) else "b is larger but 0"
   | otherwise = if a /= 0 then "1" else "a and b are both 0"
 
--- Question 2
+
+-- Question 5
 -- Write a function that takes in two numbers and calculates the sum of squares for the product and quotient
--- of those numbers. Write the function such that you use a where statement inside a let-in stamenet and a
--- let-in stamenet inside a where statement. 
+-- of those numbers. Write the function such that you use a where block inside a let expression and a
+-- let expression inside a where block. 
 
 invertedConstructions :: Double -> Double -> Double
 invertedConstructions a b = let sqrtProd = sqrt abProd where abProd = a * b
