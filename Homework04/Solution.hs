133e0da4dfdbd026d1538aba2fddec7b5ae80b83
Robertino <48034748+rober-m@users.noreply.github.com>
2022-09-26 00:57:53 +0100

    Created new questions

--- Homework04/Solution.hs
+++ Homework04/Solution.hs
@@ -1,41 +1,62 @@
 
+
 -- Question 1
--- Rewrite the function from the lesson 3A such that you use guards and case-of statements
-
-guardsAndCaseOf :: Double -> Double -> String
-guardsAndCaseOf a b
-  | a > b = case a of
-                0 -> "a is larger but 0"
-                _ -> show (a/b)
-  | a < b = case b of
-                0 -> "b is larger but 0"
-                _ -> show (b/a)
-  | otherwise = case a of
-                0 -> "a and b are both 0"
-                _ -> "1"
-
--- Question 2
--- Lets say you have the variable nested defined bellow. How would you get the value of
+-- Lets say you have the nested values defined bellow. How would you get the value of
 -- 4 by using only pattern matching in a function?
 
 nested :: [([Int], [Int])]
 nested = [([1,2],[3,4]), ([5,6],[7,8])]
 
 three :: [([Int], [Int])] -> Int
-three [([a,b],[c,d]), _] = d
+three [(_,[_,d]), _] = d
 three _ = 0
 
+-- Question 2
+-- Write a function that takes a list of elements of any type and, if the list has 3 or more elements, it
+-- removes them. Else, it does nothing. Do it two times, one with multiple function definitions and one with
+-- case expressions.
+
+remove3 :: [a] -> [a]
+remove3 (_:_:_:xs) = xs
+remove3 x          = x 
+
+remove3' :: [a] -> [a]
+remove3' list = case list of
+    (_:_:_:xs) -> xs
+    x          -> x
+
 -- Question 3
--- Write a function that takes in a list of integers and returns the number that comes
--- after the first occurence of 1. If there is no 1 in the list return a 0. Use pattern
--- matching and recursive calls for your function. 
+-- Create a function that takes a 3-element tuple (all of type Integer) and adds them together
 
-myList :: [Int]
-myList = [3,9,5,8,2,1,7,3,9,2] -- your function should return 7 for this list
+add3 (Integer, Integer, Integer) -> Integer
+add3 (x, y, z) = x + y + z
 
-after1 :: [Int] -> Int
-after1 [] = 0
-after1 [x] = 0
-after1 (x:xs) = if x == 1 
-                then head xs
-                else after1 xs
+-- Question 4
+-- Implement a function that returns True if a list is empty and False otherwise.
+
+null' :: [a] -> Bool
+null' []        = True
+null' _         = False
+
+-- or (alternative answer):
+
+null'' :: [a] -> Bool
+null'' []    =  True
+null'' (_:_) =  False
+
+-- Question 5
+-- Write the implementation of the tail function using pattern matching. But, instead of failing if
+-- the list is empty, return an empty list.
+
+tail' :: [a] -> [a]
+tail' (x:xs) = xs
+tail' []      = []
+
+-- Question 6
+-- write a case expression wrapped in a function that takes an Int and adds one if it's even. Otherwise does nothing. 
+-- (Use the `even` function to check if the number is even.)
+
+addOneIfEven :: Int -> Int
+addOneIfEven n = case even n of
+    True  -> n + 1
+    False -> n
