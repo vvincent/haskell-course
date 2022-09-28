3d080514e34bd5403672315fdb82fd3034f3ff87
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-09-28 17:52:18 +0200

    Updating homework for lesson 7.

--- Homework07/Solution.hs
+++ Homework07/Solution.hs
@@ -1,6 +1,15 @@
 
-
 -- Question 1
+-- When you want to get the second element from a tuple you can use snd function.
+-- If you have a list of tuples and would like the list of second element you can use
+-- map snd functions. Write now a function that extract the third element from every
+-- tuple element in a list of tuples. Do not use the map function.
+
+getThirdElements :: [(a, b, c)] -> [c]
+getThirdElements [] = []
+getThirdElements ((x1,x2,x3):xs) = x3 : getThirdElements xs
+
+-- Question 2
 -- Write your own version of the functions map, sum and filter. Use pattern matching. 
 
 myMap :: (a -> b) -> [a] -> [b]
@@ -16,7 +25,7 @@ myFilter f [] = []
 myFilter f (x:xs) = if f x then x : myFilter f xs
                     else myFilter f xs
 
--- Question 2
+-- Question 3
 -- Write your own version of the functions foldl and foldr. 
 
 myFoldL :: (a -> a -> a) -> a -> [a] -> a 
@@ -28,7 +37,7 @@ myFoldR :: (a -> a -> a) -> a -> [a] -> a
 myFoldR f start [x] = f x start 
 myFoldR f start xs = myFoldR f (f (last xs) start) $ init xs
 
--- Question 3
+-- Question 4
 -- Write your own version of scanl and scanr functions. You can use pattern matching.
 
 myScanL :: (a -> a -> a) -> a -> [a] -> [a]
