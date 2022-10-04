2bceebaff7474257a939358cdbf2e3bebd1338d9
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-10-04 16:09:33 +0200

    Updating homework for lesson 6.

--- Homework/Homework06/Homework06.hs
+++ Homework/Homework06/Homework06.hs
@@ -1,20 +1,20 @@
 
-
 -- Question 1
--- Change the code of the function mySort the we wrote in the lesson so that it
--- will work as the actual sort function from the Data.List module. 
+-- Write a version of the product function without using pattern matching.
 
 
 -- Question 2
+-- Write a function that takes in a list of integers and removes from it the smallest
+-- element. If the element is occuring more then once, it should remove all of them.
+
+
+-- Question 3
 -- Write a function that takes in an integer n, calculates the factorial n! and 
 -- returns a string in the form of 1*2* ... *n = n! where n! is the actual result. 
 
 
--- Question 3
+-- Question 4
 -- Write your own version of functions zip and zipWith. Use pattern matching.
--- zip takes in two lists and returns a list of tuple pairs. zipWith works similar
--- as zip just that it also takes in a function and applies it to the pair so that
--- it returns just a list of elements with the same type as the input lists.
 
 -- zip [1..3] [3..1]
 -- returns: [(1,3),(2,2),(3,1)]
