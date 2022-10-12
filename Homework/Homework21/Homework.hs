309ee5dde66efa47e4bdb0f70b69c85fdb563c88
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-10-12 12:57:16 +0200

    Updating homework for lesson 21.

--- Homework/Homework21/Homework.hs
+++ Homework/Homework21/Homework.hs
@@ -5,3 +5,16 @@
 -- is "count": n, and n represents the lenght of the Map. The function should run a Reader monad
 -- that is parameterize by this Map and returns a bool, which says weather the count number in 
 -- the map is representing the actual length of the Map. Use the asks and the ask functions.
+
+
+-- Question 2
+-- Write a program that asks the user for his name and generater a HTML document that
+-- displays a simple web-page with his name. Use the Reader monad. Below you can see 
+-- an example of the HTML document for the user User1.
+
+-- <!DOCTYPE html>
+-- <html lang="en">
+--   <body>
+--     <h1>Your site</h1><h3>Hello User1!</h3>
+--   </body>
+-- </html>
