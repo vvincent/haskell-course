2251b66c27e4305ffeb4719f0762af02b41a0b56
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-10-12 15:37:12 +0200

    Adding homework for lesson 22.

---
+++ Homework/Homework22/Homework.hs
@@ -0,0 +1,15 @@
+
+-- Question 1
+-- Rewrite the example from the lesson that uses the pass function, such that it 
+-- will use the censor function instead. Look at how the type signatures are chaning. 
+
+
+-- Question 2
+-- Rewrite the example from the lesson that uses the listen function, such that it 
+-- will use the listens function instead.
+
+
+-- Question 3
+-- Write a program that asks the user for 2 integers and then computes their greatest
+-- common divisor with Euclid’s algorithm. The programm should also write out the steps
+-- that the algorithm is performing. Use a Writer monad to acomplis this.
