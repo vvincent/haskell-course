0bc13a8abfd92afb47bad984fb571e0431d78d3c
LukaKurnjek <100196101+LukaKurnjek@users.noreply.github.com>
2022-10-12 11:19:06 +0200

    Updating homework for lesson 21.

--- Homework/Homework21/Homework.hs
+++ Homework/Homework21/Homework.hs
@@ -4,4 +4,4 @@
 -- Write a function that takes in a dictionary of type Map String Int, where the first element 
 -- is "count": n, and n represents the lenght of the Map. The function should run a Reader monad
 -- that is parameterize by this Map and returns a bool, which says weather the count number in 
--- the map is representing the actual length of the Map. 
+-- the map is representing the actual length of the Map. Use the asks and the ask functions.
