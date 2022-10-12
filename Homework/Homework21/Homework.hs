2ebb05552a3af77e37b552d29b729499046898f4
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-10-12 11:14:01 +0200

    Adding homework for lesson 21.

---
+++ Homework/Homework21/Homework.hs
@@ -0,0 +1,7 @@
+
+
+-- Question 1
+-- Write a function that takes in a dictionary of type Map String Int, where the first element 
+-- is "count": n, and n represents the lenght of the Map. The function should run a Reader monad
+-- that is parameterize by this Map and returns a bool, which says weather the count number in 
+-- the map is representing the actual length of the Map. 
