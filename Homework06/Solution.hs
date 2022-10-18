b8973158d8808c3f9424bd12a8b0089140f41ef7
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-10-18 15:33:01 +0200

    Updating solution for homework 6.

--- Homework06/Solution.hs
+++ Homework06/Solution.hs
@@ -1,14 +1,5 @@
 
 -- Question 1
--- Write a version of the product function without using pattern matching.
-
-myProduct :: [Int] -> Int
-myProduct xs = if xs == [] then 0
-               else multiply xs 
-  where multiply xs = if xs == [] then 1
-                      else head xs * multiply (tail xs)
-
--- Question 2
 -- Write a function that takes in a list of integers and removes from it the smallest
 -- element. If the element is occuring more then once, it should remove only the first
 -- occurence of the smallest element.
@@ -32,7 +23,7 @@ removeMin' xs = removeMin' (minimum xs) xs
                              then removeMin' minEl (tail ys)
                              else head ys : removeMin' minEl (tail ys)
 
--- Question 3
+-- Question 2
 -- Write a function that takes in an integer n, calculates the factorial n! and 
 -- returns a string in the form of 1*2* ... *n = n! where n! is the actual result. 
 
@@ -43,7 +34,7 @@ factorial n = accumulate 2 "1" ++ " = " ++ show result
           else accumulate (x+1) (string ++ "*" ++ show x)
         result = product [1..n]
 
--- Question 4
+-- Question 3
 -- Write your own version of functions zip and zipWith. Use pattern matching.
 
 -- zip [1..3] [3..1]
@@ -64,7 +55,7 @@ myZipWith f _ [] = []
 myZipWith f [x] [y] = [f x y]
 myZipWith f (x:xs) (y:ys) = f x y : myZipWith f xs ys
 
--- Question 5
+-- Question 4
 -- When you want to get the second element from a tuple you can use snd function.
 -- If you have a list of tuples and would like the list of second element you can use
 -- map snd functions. Write now a function that extract the third element from every
@@ -74,25 +65,7 @@ getThirdElements :: [(a, b, c)] -> [c]
 getThirdElements [] = []
 getThirdElements ((x1,x2,x3):xs) = x3 : getThirdElements xs
 
--- Question 6
--- Write your own version of foldr, scanl and scanr functions. You can use pattern matching.
--- For the foldr function use such a recursive call: foldr f v xs = foldr ...
-
-myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr _ v [] =  v 
-myFoldr f v xs = myFoldr f (f (last xs) v) $ init xs
-
-myScanL :: (a -> a -> a) -> a -> [a] -> [a]
-myScanL f x [] = [x]
-myScanL f x [y] = [f x y]
-myScanL f x (y:ys) = x : myScanL f (f x y) (tail ys)
-
-myScanR :: (a -> a -> a) -> a -> [a] -> [a]
-myScanR f x ys = reverse $ myScanR' f x ys
-  where myScanR' f x [] = [x]
-        myScanR' f x ys = x : myScanR' f (f (last ys) x) (init ys)
-
--- Question 7
+-- Question 5
 -- Below you have defined some beer prices in bevogBeerPrices and your order list in
 -- orderList + the deliveryCost. Write a function that takes in an order and calculates
 -- the cost including delivery.
@@ -104,7 +77,7 @@ bevogBeerPrices =
   ,("Ond", 8.50)
   ,("Baja", 7.50)]
 
-orderList :: [(String, Int)]
+orderList :: [(String, Double)]
 orderList = [("Tak", 5)
             ,("Kramah", 4)
             ,("Ond", 3)]
@@ -112,17 +85,15 @@ orderList = [("Tak", 5)
 deliveryCost :: Double
 deliveryCost = 8.50
 
-beerCosts :: [(String, Int)] -> Double
+beerCosts :: [(String, Double)] -> Double
 beerCosts order =
-  foldr ((+) . snd) deliveryCost
+  foldr (+) deliveryCost
+  . zipWith (*) (map snd orderList) 
+  . map snd 
   . filter (\name -> fst name `elem` map fst order) $
   bevogBeerPrices
 
-main1 :: IO ()
-main1 = do
-  print $ beerCosts orderList
-
--- Question 8
+-- Question 6
 -- Write a function that takes in a integer and returns a list of all prime numbers
 -- that are smaller or equal to the input number. Use recursion, filter and map for it.
 
@@ -136,6 +107,15 @@ primes n = if n < 2 then []
         checkPrimality :: [Int] -> Int -> Bool
         checkPrimality xs x = length (filter (== 0) (map (rem x) xs)) == 0
 
-main2 :: IO ()
-main2 = do
-  print $ primes 15
+-- Question 7 (Extra credits)
+-- Write your own version of scanl and scanr functions. You can use pattern matching.
+
+myScanL :: (a -> a -> a) -> a -> [a] -> [a]
+myScanL f x [] = [x]
+myScanL f x [y] = [f x y]
+myScanL f x (y:ys) = x : myScanL f (f x y) (tail ys)
+
+myScanR :: (a -> a -> a) -> a -> [a] -> [a]
+myScanR f x ys = reverse $ myScanR' f x ys
+  where myScanR' f x [] = [x]
+        myScanR' f x ys = x : myScanR' f (f (last ys) x) (init ys)
