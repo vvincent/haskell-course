16ab510ddfbec44733be29627b25277c661e1522
Robertino <48034748+rober-m@users.noreply.github.com>
2022-10-19 20:12:04 +0100

    changed homework

--- Homework/Homework06/Homework06.hs
+++ Homework/Homework06/Homework06.hs
@@ -1,61 +1,106 @@
+-- Warm up question.
+-- Write a function called `null'` that checks if a lists is empty.
+
 
 -- Question 1
--- Write a function that takes in a list of integers and removes from it the smallest
--- element. If the element is occurring more then once, it should remove only the first
--- occurrence of the smallest element.
-
--- Challenge: Write the this function in such a way that if you add some code to it and
--- not remove any, the function will be removing all instances of the smallest element.
+-- Write a function called `repeat'` that takes a value and creates an infinite list with
+-- the value provided as every element of the list.
+--
+-- >>> repeat 17
+--[17,17,17,17,17,17,17,17,17...
 
 
 -- Question 2
--- Write a function that takes in an integer n, calculates the factorial n! and 
--- returns a string in the form of 1*2* ... *n = n! where n! is the actual result. 
+-- Using the `repeat'` function and the `take` function we defined in the lesson (comes with Haskell),
+-- create a function called `replicate'` that takes a number `n` and a value `x` and creates a list
+-- of length `n` with `x` as the value of every element. (`n` has to be Integer.)
+--
+-- >>> replicate 0 True
+-- []
+-- >>> replicate (-1) True
+-- []
+-- >>> replicate 4 True
+-- [True,True,True,True]
 
 
 -- Question 3
--- Write your own version of functions zip and zipWith. Use pattern matching.
-
--- zip [1..3] [3..1]
--- returns: [(1,3),(2,2),(3,1)]
-
--- zipWith (+) [1,2,3] [1,2,3]
--- returns: [2,4,6]
+-- Write a function called `concat'` that concatenates a list of lists.
+--
+-- >>> concat' [[1,2],[3],[4,5,6]]
+-- [1,2,3,4,5,6]
 
 
 -- Question 4
--- When you want to get the second element from a tuple you can use snd function.
--- If you have a list of tuples and would like the list of second element you can use
--- map snd functions. Write now a function that extract the third element from every
--- tuple element in a list of tuples. Do not use the map function.
+-- Write a function called `zip'` that takes two lists and returns a list of
+-- corresponding pairs (zips them) like this:
+--
+-- >>> zip' [1, 2] ['a', 'b']
+-- [(1,'a'),(2,'b')]
+--
+-- If one input list is shorter than the other, excess elements of the longer
+-- list are discarded, even if one of the lists is infinite:
+--
+-- >>> zip' [1] ['a', 'b']
+-- [(1,'a')]
+-- >>> zip' [1, 2] ['a']
+-- [(1,'a')]
+-- >>> zip' [] [1..]
+-- []
+-- >>> zip' [1..] []
+-- []
+
 
 
 -- Question 5
--- Below you have defined some beer prices in bevogBeerPrices and your order list in
--- orderList + the deliveryCost. Write a function that takes in an order and calculates
--- the cost including delivery.
-
-bevogBeerPrices :: [(String, Double)]
-bevogBeerPrices =
-  [("Tak", 6.00)
-  ,("Kramah", 7.00)
-  ,("Ond", 8.50)
-  ,("Baja", 7.50)]
-
-orderList :: [(String, Double)]
-orderList = [("Tak", 5)
-            ,("Kramah", 4)
-            ,("Ond", 3)]
-
-deliveryCost :: Double
-deliveryCost = 8.50
+-- Create a function called `zipWith'` that generalises `zip'` by zipping with a
+-- function given as the first argument, instead of a tupling function.
+--
+-- > zipWith' (,) xs ys == zip' xs ys
+-- > zipWith' f [x1,x2,x3..] [y1,y2,y3..] == [f x1 y1, f x2 y2, f x3 y3..]
+--
+-- For example, `zipWith' (+)` is applied to two lists to produce the list of
+-- corresponding sums:
+--
+-- >>> zipWith (+) [1, 2, 3] [4, 5, 6]
+-- [5,7,9]
 
 
 -- Question 6
--- Write a function that takes in a integer and returns a list of all prime numbers
--- that are smaller or equal to the input number. Use recursion, filter and map for it.
+-- Write a function called `takeWhile'` that takes a precate and a list and
+-- returns the list up until an element that doesn't satisfy the predicate.
+--
+-- >>> takeWhile (< 3) [1,2,3,4,1,2,3,4]
+-- [1,2]
+-- >>> takeWhile (< 9) [1,2,3]
+-- [1,2,3]
+-- >>> takeWhile (< 0) [1,2,3]
+-- []
 
 
--- Question 7 (Extra credits)
--- Write your own version of scanl and scanr functions. You can use pattern matching.
+-- Question 7 (More difficult)
+-- Write a function that takes in an integer n, calculates the factorial n! and
+-- returns a string in the form of 1*2* ... *n = n! where n! is the actual result.
 
+
+-- Question 8
+-- Below you have defined some beer prices in bevogBeerPrices and your order list in
+-- orderList + the deliveryCost. Write a function that takes in an order and calculates
+-- the cost including delivery. Assume that the two lists have the beers in the same order.
+
+bevogBeerPrices :: [(String, Double)]
+bevogBeerPrices =
+  [ ("Tak", 6.00),
+    ("Kramah", 7.00),
+    ("Ond", 8.50),
+    ("Baja", 7.50)
+  ]
+
+orderList :: [(String, Double)]
+orderList =
+  [ ("Tak", 5),
+    ("Kramah", 4),
+    ("Ond", 7)
+  ]
+
+deliveryCost :: Double
+deliveryCost = 8.50
