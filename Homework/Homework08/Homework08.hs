f7f4984e755b9ada5b6957b1284a37c3efea7bbd
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-10-20 15:22:35 +0200

    Updating homework for lesson 8.

--- Homework/Homework08/Homework08.hs
+++ Homework/Homework08/Homework08.hs
@@ -1,21 +1,23 @@
 
--- This statement is a language pragma about which you will learn in lesson 12. We use it
+-- This statement is a language pragma about which you will learn in lesson 11. We use it
 -- here so we can have same function names in record syntax definitions of different types.
 {-# LANGUAGE DuplicateRecordFields #-}
 
 -- Question 1
 -- Algebraic data types are types combined of other types. You have 2 choices for combining.
+
 -- The first one is called product types where you combine types with a logical "and":
 -- type Address = String
 -- type Number = Int
 -- data productType = Address Number
+
 -- The second one is called sum types where you combine types with a logical "or":
 -- type sumType = Descriptive Address | Numeric Number
 
--- Below you have a data type defined and you want to add another data type to your code.
--- Solve the problem in 2 ways. First use only product types where you extract the common
--- parameters and define a new type that you then use for constructing both types. Second
--- use sum types to define the base common type. Try to solve the sum type in various ways.
+-- Below you have two data types defined. Try to make a general type, where you extract the 
+-- common properties of both and then use it when you define those two types as product types.
+-- Write then a general Instruments sum type that allows the user to define same data as in the
+-- initial instrument data types. Try to find more then one solution.
 
 data Guitar = Guitar { brand :: String
                      , price :: Float
@@ -25,7 +27,8 @@ data Drums = Drums { brand :: String
                    , price :: Float
                    , drumCount :: Int }
 
+
 -- Question 2
 -- Try to implement a data type that in its definition is refering to itself and make an
--- instace of it. And example of this is the linked list that you have saw in the lesson. 
--- Here is another way to define it: data List a = Empty | Cons a (List a) deriving Show 
+-- instace of it. And example of this is the implementation of the list data type.
+
