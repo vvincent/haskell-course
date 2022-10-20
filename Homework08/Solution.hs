6aefe28d96288c2116f0e9190ce8964674da374d
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-10-20 15:17:12 +0200

    Updating solution for homework 8.

--- Homework08/Solution.hs
+++ Homework08/Solution.hs
@@ -1,26 +1,28 @@
 
--- This statement is a language pragma about which you will learn in lesson 12. We use it
+-- This statement is a language pragma about which you will learn in lesson 11. We use it
 -- here so we can have same function names in record syntax definitions of different types.
 {-# LANGUAGE DuplicateRecordFields #-}
 
 -- This language extension and import statement are used for handling ByteStrings about
--- which you will learn in lesson 11
+-- which you will learn in lesson 12
 {-# LANGUAGE OverloadedStrings #-}
 import Data.ByteString (ByteString)
 
 -- Question 1
 -- Algebraic data types are types combined of other types. You have 2 choices for combining.
+
 -- The first one is called product types where you combine types with a logical "and":
 -- type Address = String
 -- type Number = Int
 -- data productType = Address Number
+
 -- The second one is called sum types where you combine types with a logical "or":
 -- type sumType = Descriptive Address | Numeric Number
 
--- Below you have a data type defined and you want to add another data type to your code.
--- Solve the problem in 2 ways. First use only product types where you extract the common
--- parameters and define a new type that you then use for constructing both types. Second
--- use sum types to define the base common type. Try to solve the sum type in various ways.
+-- Below you have two data types defined. Try to make a general type, where you extract the 
+-- common properties of both and then use it when you define those two types as product types.
+-- Write then a general Instruments sum type that allows the user to define same data as in the
+-- initial instrument data types. Try to find more then one solution.
 
 data Guitar = Guitar { brand :: String
                      , price :: Float
@@ -30,9 +32,9 @@ data Drums = Drums { brand :: String
                    , price :: Float
                    , drumCount :: Int }
 
--- First solution
-data InstrumentData = InsData { brand :: String
-                              , price :: Float }
+-- Product type solution
+data InstrumentData = InstData { brand :: String
+                               , price :: Float }
 
 type GuitarColor = String
 data Guitar1 = Guitar1 InstrumentData GuitarColor
@@ -40,7 +42,7 @@ data Guitar1 = Guitar1 InstrumentData GuitarColor
 type DrumsCount = Int
 data Drums1 = Drums1 InstrumentData DrumsCount
 
--- Second solution
+-- Sum type solution
 data Instrument1 = Guitar2 InstrumentData GuitarColor | Drums2 InstrumentData DrumsCount
 -- or
 data Instrument2 = Guitar3 { brand :: String
@@ -55,15 +57,13 @@ data Instrument3 = Instrument3 InstrumentData AdditionalData
 
 -- Question 2
 -- Try to implement a data type that in its definition is refering to itself and make an
--- instace of it. And example of this is the linked list that you have saw in the lesson. 
--- Here is another way to define it: data List a = Empty | Cons a (List a) deriving Show 
+-- instace of it. And example of this is the implementation of the list data type.
 
--- We will show here the Plutus data type called Data that has the following definition:
-data Data = Constr Integer [Data]
-          | Map [(Data, Data)]
-          | List [Data]
-          | I Integer
-          | B ByteString
+data MyData = Map [(MyData, MyData)]
+            | List [MyData]
+            | I Integer
+            | S String
+            deriving Show
 
-myData :: Data
-myData = Map [(I 1, B "one"), (I 2, B "two"), (I 3, B "three")]
+myData :: MyData
+myData = Map [(I 1, S "one"), (I 2, S "two"), (I 3, S "three")]
