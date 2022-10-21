80813a12644afd6367347503effd58bb91a08dc2
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-10-21 11:19:42 +0200

    Updating solution for homework 10.

--- Homework10/Solution.hs
+++ Homework10/Solution.hs
@@ -1,34 +1,39 @@
-import System.Directory (createDirectory, removeDirectory, listDirectory)
-import Data.Time.Clock
+
+import System.Directory ( listDirectory )
+import Data.Time.Clock ( diffUTCTime, getCurrentTime )
 
 -- Question 1
--- Define an IO action that print the string "Hello World".
+-- Define an IO action that count the amount of files in the current directory 
+-- and prints it to the terminal.
 
-helloWorldPrint :: IO ()
-helloWorldPrint = putStrLn "Hello World"
+listFiles :: IO ()
+listFiles = do 
+    x <- listDirectory "."
+    let amount = length x
+    putStrLn $ "Files and folders in current directory: " ++ show amount
 
 -- Question 2
--- Define an IO action that has as input a name from StdIn, and returns "Hello {NAME HERE}".
+-- Define an IO action that calculates the time an other IO action takes. 
+-- HINT: use getCurrentTime and diffUTCTime from the module Data.Time.Clock
 
-helloName :: IO ()
-helloName = getLine >>= putStrLn . ("Hello " ++) 
+timeIO :: IO a -> IO ()
+timeIO io = do 
+    initTime <- getCurrentTime
+    result <- io
+    finalTime <- getCurrentTime
+    let diff = diffUTCTime finalTime initTime
+    putStrLn $ "Time used for io action is: " ++ show diff
 
 -- Question 3
--- Define an IO action that count the amount of files in the current directory.
+-- Write an IO action that asks the user to type something and the program then 
+-- writes the message to a file called msg.txt. After that it reads the text from
+-- the msg.txt file and prints it back. Use the writeFile and readFile functions.
 
-listAction :: IO Int
-listAction = do x <- listDirectory "."
-                let amount = length x
-                return amount
-
--- Question 4
--- Define an IO action that calculates the time an other IO action takes. HINT: use getCurrentTime and diffUTCTime
--- from the module Data.Time.Clock
-
-timeIO :: IO a -> IO a
-timeIO a = do initTime <- getCurrentTime
-              result <- a
-              finalTime <- getCurrentTime
-              let diff = diffUTCTime finalTime initTime
-              print diff
-              return result
+createMsg :: IO ()
+createMsg = do
+    putStrLn "Type in a message:"
+    msg <- getLine
+    writeFile "./msg.txt" msg
+    putStrLn "Wrote message to msg.txt file."
+    fileContent <- readFile "./msg.txt"
+    putStrLn $ "File content is: " ++ fileContent
