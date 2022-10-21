0d9289b0f0f8557aabd2df7cbbfbf0a4b6e79eeb
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-10-21 11:23:29 +0200

    Updating homework for lesson 10.

--- Homework/Homework10/Homework10.hs
+++ Homework/Homework10/Homework10.hs
@@ -1,20 +1,19 @@
-import System.Directory (createDirectory, removeDirectory, listDirectory)
-import Data.Time.Clock
+
+import System.Directory ( listDirectory )
+import Data.Time.Clock ( diffUTCTime, getCurrentTime )
 
 -- Question 1
--- Define an IO action that print the string "Hello World".
+-- Define an IO action that count the amount of files in the current directory 
+-- and prints it to the terminal.
 
 
 -- Question 2
--- Define an IO action that has as input a name from StdIn, and returns "Hello {NAME HERE}".
+-- Define an IO action that calculates the time an other IO action takes. 
+-- HINT: use getCurrentTime and diffUTCTime from the module Data.Time.Clock
 
 
 -- Question 3
--- Define an IO action that count the amount of files in the current directory.
-
-
--- Question 4
--- Define an IO action that calculates the time an other IO action takes. HINT: use getCurrentTime and diffUTCTime
--- from the module Data.Time.Clock
-
+-- Write an IO action that asks the user to type something and the program then 
+-- writes the message to a file called msg.txt. After that it reads the text from
+-- the msg.txt file and prints it back. Use the writeFile and readFile functions.
 
