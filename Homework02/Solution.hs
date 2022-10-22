ced2247c1d45656a29131ac8216fea1d5461d55e
Robertino <48034748+rober-m@users.noreply.github.com>
2022-10-22 19:33:04 +0100

    fixed homework

--- Homework02/Solution.hs
+++ Homework02/Solution.hs
@@ -15,44 +15,36 @@ f5 :: String -> String -> String -> Bool
 f5 x y z = x == (y ++ z)
 
 -- Question 2
--- Are really all variables in Haskell immutable? Try googling for the answer.
-
--- Not all variables are immutable. An example is the data type STUArray which stands for stateful unboxed array. 
--- But you can not just declare it as a normal variable. It can be only created inside a function and then returned 
--- as a normal unboxed array which is immutable. So there are still some restrictions in Haskell what you can do
--- with mutable data structures.  
-
--- Question 3
 -- Why should we define type signatures of functions? How can they help you? How can they help others?
 
 -- Type signatures place restrictions on your data and can help you to catch errors you make in your code. Also
--- you can develop your programs by first defining the type signatures of functions which sets the data transformation
--- flow. And after that you just code the details. They can also help other developers to better understand your code.
+-- you can develop your programs by first defining the function's types, which sets the data transformation
+-- flow. After that, it's easier to implement the functions.
+-- They also help other developers (and your self) to better understand your code.
 
--- Question 4
+-- Question 3
 -- Why should you define type signatures for variables? How can they help you?
 
 -- In Haskell we ussually declare variables inside functions. We mostly do no use type signatures for them. But in the
 -- begining when you are getting errors it is helpful to add the signatures to all of your variables because the error
 -- from the compiler can become more understandable if you do this.
 
--- Question 5
+-- Question 4
 -- Are there any functions in Haskell that let you transform one type to the other? Try googling for the answer.
 
--- The function show and read transform a string to a numeric type and vice versa:
+-- We can use the function `show` to transform almost any type into String (we'll learn specifically which types in the type classes lesson):
+
 v1 :: Float
 v1 = 1.23
 
 v2 :: String
 v2 = show v1
 
-v3 = read v2 :: Double -- another way of adding type signatures to variables
-
--- You can also convert an Double to an Int with the round function.
+-- You can also convert a `Double` to an `Int` with the round function (although, you're loosing information).
 v4 = 3.1415 :: Double
 v5 = round v4 :: Int
 
--- Question 6
+-- Question 5
 -- Can you also define in Haskell list of lists? Did we showed any example of that? How would you access the inner
 -- most elements?
 
