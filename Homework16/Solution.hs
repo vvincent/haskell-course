fd6fa4562de0aeee20c0c8a3034df7bcc9a58417
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-11-02 15:21:20 +0100

    Updating solution for homework 16.

--- Homework16/Solution.hs
+++ Homework16/Solution.hs
@@ -1,6 +1,6 @@
 
 -- Question 1
--- Take the book code exple from the lesson and write a function that askes the user to
+-- Take the book code exple from the lesson and write a function that asks the user to
 -- input an index (1, 2 or 3) and returns the title of the book by using the <$> operator.
 -- The code you need is defined below. If it can't get the title write a message.
 
@@ -36,13 +36,13 @@ books = Map.fromList $ zip [1..3] [book1, book2, book3]
 
 getTitle :: IO ()
 getTitle = do
-    print "Input an index:"
+    putStrLn "Input an index:"
     ind <- read <$> getLine
     let book = Map.lookup ind books
     let myTitle = title <$> book
     if isJust myTitle then
-        print $ "The title is:\n" ++ fromJust myTitle
-    else print $ "could not find the book with index: " ++ show ind
+        putStrLn $ "The title is:\n" ++ fromJust myTitle
+    else putStrLn $ "Could not find the book with index: " ++ show ind
 
 -- Question 2
 -- For the Wrapper example make an instance of Functor for the Wrapper type. Then write 
@@ -63,14 +63,14 @@ instance Functor Wrapper where
 
 createWrapper :: IO ()
 createWrapper = do
-    print "Input a number:"
+    putStrLn "Input a number:"
     n <- (read <$> getLine) :: IO Double
     print $ add1 <$> (<$) n (Wrapper 0)
 
 -- Question 3
 -- Implement a linked list data type (a list that is build similar as Haskell lists are - 
 -- with consing) and then define an Functor instance for this List data type. Then create
--- a list from this type and apply the add1 function from the previous question to it.
+-- a list that has this type and apply the add1 function from the previous question to it.
 
 data List a = Null | Cons a (List a) deriving Show 
 
