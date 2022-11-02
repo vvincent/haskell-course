618711b41e752cfa81f4868d759c2811d97d0b39
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-11-02 15:24:07 +0100

    Updating homework for lesson 16.

--- Homework/Homework16/Homework16.hs
+++ Homework/Homework16/Homework16.hs
@@ -1,7 +1,7 @@
 
 -- Question 1
--- Take the book code exple from the lesson and write a function that askes the user to
--- inout an index (1, 2 or 3) and returns the title of the book by using the <$> operator.
+-- Take the book code exple from the lesson and write a function that asks the user to
+-- input an index (1, 2 or 3) and returns the title of the book by using the <$> operator.
 -- The code you need is defined below. If it can't get the title write a message.
 
 import qualified Data.Map as Map
@@ -34,6 +34,7 @@ book3 = Book {
 books :: Map.Map Int Book
 books = Map.fromList $ zip [1..3] [book1, book2, book3]
 
+
 -- Question 2
 -- For the Wrapper example make an instance of Functor for the Wrapper type. Then write 
 -- a function that asks the user to input a number (can be also decimal), creates a Wraper 
@@ -44,7 +45,10 @@ data Wrapper a = Empty | Wrapper a deriving Show
 add1 :: Num a => a -> a
 add1 n = n + 1
 
+
 -- Question 3
 -- Implement a linked list data type (a list that is build similar as Haskell lists are - 
 -- with consing) and then define an Functor instance for this List data type. Then create
--- a list from this type and apply the add1 function from the previous question to it.
+-- a list that has this type and apply the add1 function from the previous question to it.
+
+
