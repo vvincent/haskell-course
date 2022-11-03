474f21e39a3ae856a2b436dc54ba7b70a9acbcd8
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-11-03 11:41:07 +0100

    Updating homework 21.

--- Homework/Homework21/Homework.hs
+++ Homework/Homework21/Homework.hs
@@ -1,12 +1,13 @@
 
 -- Question 1
--- Rewrite the example from the lesson that uses the pass function, such that it 
--- will use the censor function instead. Look at how the type signatures are chaning. 
+-- Rewrite the example from the lesson that sums the two ages and uses the pass function, 
+-- such that it will use the censor function instead. Notice how the type signatures of
+-- the function sumAge gets simplified.
 
 
 -- Question 2
--- Rewrite the example from the lesson that uses the listen function, such that it 
--- will use the listens function instead.
+-- Rewrite the example from the lesson that uses the listen function, when reading the
+-- logs from the start function such that it will use the listens function instead.
 
 
 -- Question 3
