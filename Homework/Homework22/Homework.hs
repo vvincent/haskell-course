13393c2bf04049dae1ce934ed5944e6ce5bf3e30
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-11-04 09:35:36 +0100

    Adding homework for lesson 22.

--- Homework/Homework22/Homework.hs
+++ Homework/Homework22/Homework.hs
@@ -1,6 +1,17 @@
 
-
--- Question 1
+{-
+Question 1
+Write a program for creating a shopping list, where the user can add 3 kinds of
+flowers. When the program is started it should display the following message:
+    Possible flowers are: daisy, sunflower and tulip.
+    Possible actions are:
+        add  --flower --amount 
+        remove --flower --amout 
+        show_list 
+If the user does not use a valid comamnd and appropriate options notify him about
+this. When the user says show_list print a message of how many of which flowers are
+added to the list. Use a state monad when coding the solution.
+-}
 
 
 
