0c6eeb93235e2d5b9ccf30ec75e07c58c4e6ed07
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-11-04 09:33:00 +0100

    Updating solution for homerowk 22.

--- Homework22/Solution.hs
+++ Homework22/Solution.hs
@@ -1,10 +1,21 @@
 
--- Question 1
--- TODO
+{-
+Question 1
+Write a program for creating a shopping list, where the user can add 3 kinds of
+flowers. When the program is started it should display the following message:
+    Possible flowers are: daisy, sunflower and tulip.
+    Possible actions are:
+        add  --flower --amount 
+        remove --flower --amout 
+        show_list 
+If the user does not use a valid comamnd and appropriate options notify him about
+this. When the user says show_list print a message of how many of which flowers are
+added to the list. Use a state monad when coding the solution.
+-}
 
-import Data.Map as Map
-import Control.Monad.State
-import Data.Maybe
+import Data.Map as Map ( fromList, insert, lookup, Map )
+import Control.Monad.State ( evalState, runState, MonadState(put, get), State )
+import Data.Maybe ( fromJust )
 import Text.Read (readMaybe)
 
 main :: IO ()
@@ -13,7 +24,7 @@ main = do
     putStrLn "Possible actions are:\n \
             \ add  --flower --amount \n \
             \ remove --flower --amout \n \
-            \ list_order \n"
+            \ show_list \n"
 
     let emptyBasket = Map.fromList [("daisy", 0),("sunflower",0),("tulip",0)]
     shop emptyBasket
@@ -38,7 +49,7 @@ shop basket = do
                 let (msg, (newBasket,_,_)) = runState updateOrder (basket, command, options)
                 putStrLn msg
                 shop newBasket
-            "list_order" -> do
+            "show_list" -> do
                 print $ evalState updateOrder (basket, command, options)
                 shop basket
             _ -> do
@@ -48,41 +59,43 @@ shop basket = do
 updateOrder :: State (Map.Map String Int, String, [String]) String
 updateOrder = do
     (basket, command, options) <- get
-    if command == "list_order"
+    if command == "show_list"
     then return $ describeBasket basket
     else do
         if length options /= 2
         then return "Options for this command should equal to 2."
         else do
             let flower = options !! 0
-                amount = readMaybe (options !! 0) :: Maybe Int
+                amount = readMaybe (options !! 1) :: Maybe Int
             if flower `notElem` ["daisy","sunflower","tulip"] || amount == Nothing
             then return "The options for this command are not correct."
             else do
+                let changeAmount = fromJust amount
+                    currentAmount = fromJust $ Map.lookup flower basket
                 case command of
                     "add" -> do
-                        let updatedbasket = Map.insert flower (fromJust amount + 1) basket
+                        let updatedbasket = Map.insert flower (changeAmount + currentAmount) basket
                         put (updatedbasket, command, options)
                         return "Updated basket."
                     "remove" -> do
-                        let changeAmount = fromJust amount
-                            currentAmount = fromJust $ Map.lookup flower basket
-                        if currentAmount - changeAmount < 0
+                        let updatedAmount = currentAmount - changeAmount
+                        if updatedAmount < 0
                         then do
                             let updatedbasket = Map.insert flower 0 basket
                             put (updatedbasket, command, options)
                             return "Updated basket."
                         else do
-                            let updatedbasket = Map.insert flower (fromJust amount - 1) basket
+                            let updatedbasket = Map.insert flower updatedAmount basket
                             put (updatedbasket, command, options)
                             return "Updated basket."
                     _ -> return "This case will anyway never happen"
 
 describeBasket :: Map.Map String Int -> String
-describeBasket basket = "The basket contains " ++ 
+describeBasket basket = "The shopping list contains " ++ 
                         show daisy ++ " daisies, " ++
                         show sunflower ++ " sunflowers and " ++
                         show tulip ++ " tulips."
   where daisy = fromJust $ Map.lookup "daisy" basket
         sunflower = fromJust $ Map.lookup "sunflower" basket
         tulip = fromJust $ Map.lookup "tulip" basket
+        
