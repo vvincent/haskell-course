5c7554f0ec4d49657e32194c172505abf2944e80
Robertino <48034748+rober-m@users.noreply.github.com>
2022-11-05 12:38:01 +0000

    Updated homework

--- Homework07/Solution.hs
+++ Homework07/Solution.hs
@@ -1,26 +1,10 @@
-
 -- Question 1
--- Check out some of the functions that are defined in the type classes stated in the
--- section "Other important type classes" from lesson 7. What do they do?
+-- Investigate the `Bounded` type class. What behaviours provides?
 
--- For the Show type class the show function can transform a variable to a string type
--- show :: Show a => a -> String
+-- The Bounded type class class is used to name the upper and lower limits of a type.
+-- It provides the minBound and maxBound behaviours to check the minimum and maximum values of a type.
 
--- For the Read type class the read function can transform a string to a number type 
--- as Int, Float, Double or another type as e.g. Bool
--- read :: Read a => String -> a
-
--- For the Enum type class the toEnum and fromEnum functions can convert user defined
--- data types that implement Enum from an Int and to an Int.
--- toEnum :: Enum a => Int -> a
--- fromEnum :: Enum a => a -> Int
-
--- For the Bounded type class the parameters maxBound and minBound give the highest
--- and smallest possible value of a certain type.
 -- maxBound :: Int -- returns 9223372036854775807
-
--- For the foldable type class the function foldl, foldr, minimum, maximum, lenght
--- and sum are very useful that we have covered in lesson 6.
 
 -- Question 2
 -- The types Int and Word bellong to the same type classes. What is the difference
@@ -31,27 +15,36 @@
 -- minBound :: Int -- returns -9223372036854775808
 -- minBound :: Word -- returns 0
 
--- The Word type has a 2 times higher maxBound and 0 for minBound which makes it as
--- a usigned Int type. 
+-- The Word type is an unsigned integral type, with the same size as Int.
+-- It has a 2 times higher maxBound and 0 for minBound.
 
 -- Question 3
--- Add type signatures to the functions below and use type variables and type classes. 
+-- Investigate the `Enum` type class. What behaviours provides?
+
+-- The Enum class defines operations on sequentially ordered types.
+-- Two important behaviors of this class are:
+
+-- `succ` that gives you the successor of a value.
+-- `pred` that gives you the predecessor of a value.
+
+-- This type class is the one that allows us to create ranges of values like [3..] and ['a'..'h']
+
+-- Question 4
+-- Add type signatures to the functions below and use type variables and type classes.
 -- Then uncomment the functions and try to compile.
 
-import Data.Char ( isDigit )
-
 f1 :: (Show a, Fractional a) => a -> a -> [Char] -> [Char]
-f1 x y z = show (x/y) ++ z
+f1 x y z = show (x / y) ++ z
 
-f2 :: (Show a, Fractional a, Read a, Enum a) => String -> a -> a -> String
-f2 x y z = if all isDigit x
-           then show $ foldl (/) (read x) [y..z]
-           else "Error: can not parse 1st parameter."
+f2 :: (Bounded a, Enum a, Eq a) => a -> a
+f2 x = if x == maxBound then minBound else succ x
 
-f3 :: (Bounded a, Enum a, Eq a) => a -> a
-f3 x = if x == maxBound
-       then minBound
-       else succ x
+-- If you try to apply f2 to a number, you'll get an "Ambiguous type variable" error because
+-- it doesn't know which numeric type you're passing and not all numeric types are instances of Bounded.
+-- You could solve this by specifying that the number is an Int or Word, for example.
 
--- The f3 function compiles but when giving a number as an input you have to add the 
--- type signature :: Int or :: Word to not get an "Ambiguous type variable" error.
+-- Question 5
+-- Investigate the numeric type classes to figure out which behaviors they provide to change between numeric types.
+
+-- We could use and combine several behaviors: `fromInteger`, `toInteger`, and `fromRational`. Although other
+-- type classes we didn't cover provide `round` `ceiling`, etc. that we can also use to go from fractions to Integrals.
