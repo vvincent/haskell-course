da44011d158bd8eec6e2ae8eb75eecca0a61a368
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-11-08 12:22:13 +0100

    Review of homework 12.

--- homework/Homework12/Homework12.hs
+++ homework/Homework12/Homework12.hs
@@ -1,4 +1,8 @@
-import Data.ByteString as BS
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.ByteString as BS ( ByteString, pack, unpack )
+import Data.ByteString.Char8 as BC ( unpack )
 
 {-
 Create a function that converts the binary representation of a ByteString into an Integer using a base 256 conversion.
@@ -7,7 +11,7 @@ Example: we can convert [4,255,50] in base 256 to base 10 as
 
                                 50*(256)^0 + 255*(256)^1 + 4*(256)^2. 
 
-Hint: since Haskell byte strings are a list of Word8 (base 256 integers) (see the toInteger :: Word8 -> Integer). 
+Hint: since Haskell byte strings are a list of Word8 (base 256 integers) check out: toInteger :: Word8 -> Integer. 
 -}
 
 byteStringToInteger :: BS.ByteString -> Integer
@@ -15,7 +19,12 @@ byteStringToInteger b = let wrds = BS.unpack b
                         in  go wrds  
                         where 
                             go [] = 0
-                            go xs = toInteger((xs !! ((Prelude.length xs) - 1))) + 256 * go (Prelude.init xs)
+                            go xs = toInteger(xs !! (Prelude.length xs - 1)) + 256 * go (Prelude.init xs)
+
+main1 :: IO ()
+main1 = do
+    let hello = "Hello World!" :: BS.ByteString
+    print $ byteStringToInteger hello
 
 {-
 Create a function that converts Integers into the binary representation using a base 256 conversion.
@@ -26,11 +35,17 @@ Example: we can convert 3000000 in base 10 to base 256 as
 
         [((300000 `div` 256) `div` 256) `mod` 256,(300000 `div` 256) `mod` 256, 300000 `mod` 256]. 
 
-Hint: since Haskell byte strings are a list of Word8 (base 256 integers) (see the toEnum :: Int -> Word8). 
+Hint: since Haskell byte strings are a list of Word8 (base 256 integers) check out: toEnum :: Int -> Word8. 
 -}
 
 integerToByteString :: Integer -> BS.ByteString
 integerToByteString n = BS.pack $ go (abs n)
                         where
                             go 0 = []
-                            go m = go (m `div` 256) ++ [(toEnum . fromIntegral)(m `mod` 256)] 
+                            go m = go (m `div` 256) ++ [(toEnum . fromIntegral) (m `mod` 256)] 
+
+main2 :: IO ()
+main2 = do
+    let hello = "Hello World!" :: BS.ByteString
+        integerHello = byteStringToInteger hello
+    print $ BC.unpack $ integerToByteString integerHello
