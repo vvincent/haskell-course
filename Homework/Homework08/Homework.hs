44ffea165e522cc7fe582434a7ae0dde980153bd
perturbing <thomas.vellekoop@iohk.io>
2022-11-16 12:03:44 +0100

    add homework lesson 8

--- Homework/Homework08/Homework.hs
+++ Homework/Homework08/Homework.hs
@@ -1,33 +1,43 @@
-
--- This statement is a language pragma about which you will learn in lesson 11. We use it
--- here so we can have same function names in record syntax definitions of different types.
+-- This homework is around creating Haskell types that represent wines from over the world.
 
 -- Question 1
--- Algebraic data types are types combined of other types. You have 2 choices for combining.
-
--- The first one is called product types where you combine types with a logical "and":
--- type Address = String
--- type Number = Int
--- data productType = Address Number
-
--- The second one is called sum types where you combine types with a logical "or":
--- type sumType = Descriptive Address | Numeric Number
-
--- Below you have two data types defined. Try to make a general type, where you extract the 
--- common properties of both and then use it when you define those two types as product types.
--- Write then a general Instruments sum type that allows the user to define same data as in the
--- initial instrument data types. Try to find more then one solution.
-
-data Guitar = Guitar { brand :: String
-                     , price :: Float
-                     , color :: String }
-
-data Drums = Drums { brand :: String
-                   , price :: Float
-                   , drumCount :: Int }
-
+-- Different wines are made from different grapes, there are around 10000 varieties over the world!
+-- Create a type synonym called "Grape" for the different grape names as strings.
+-- Additionally, use this type synonym for the grapes: "Sangiovese", "Cabernet-sauvignon", "Merlot" and "Garnacha".
 
 -- Question 2
--- Try to implement a data type that in its definition is refering to itself and make an
--- instace of it. And example of this is the implementation of the list data type.
+-- The most famous regions that export wine are located in France, Italy and Spain.
+-- Each of these countries is divided up in smaller regions.
+-- These smaller regions are known for a certain style, for example the Champagne region in France
+-- Create a type synonym called "Region" for wine region given their country and region as a tuple of strings.
+-- Additionally, use this type synonym for the regions: Bordeaux in France, Tuscany in Italy and Rioja in Spain.
 
+-- Question 3
+-- A wine is either one of three kinds, these are red, white or rose wine.
+-- Besides its kind, each wine also has a given alcohol level.
+-- Create a data type called "Kind" that represents these three kinds, with each capturing the level of alcohol.
+-- Additionally, use this data type for the examples: red wine with 14.5% alcohol, white wine with 13% alcohol 
+-- and Rose wine with 12% alcohol.
+
+-- Question 4
+-- In the world of wines, bottles display all of the above information for the consumer on its label.
+-- Create a record type called "Label" that captures the grapes that are in a whine, the region its from,
+-- and it's kind. Notice that some wines are a blended combination of multiple grapes!
+-- Additionally, create for each of the described wine below a label.
+
+-- Larrosa Rose is a rose wine from the region Rioja. It is made from the Garnacha grape and 
+-- has a alcohol level of 14%.
+
+-- Castiglioni is a red wine from the region of Tuscany. It is made from the grape Sangiovese and
+-- has an alcohol level of 12.5%.
+
+-- Bordeaux is known for its red wine, these are mainly a blend between Cabernet-sauvignon and Merlot.
+-- Create a Label for the wine "Le Petit Haut Lafitte" that has an alcohol percentage 13.5%.
+
+-- Question 5
+-- Write a function `containsGrape` that takes a list of Labels and a Grape and returns a boolean.
+-- The function should check if the there exists a wine in the Label that contains this Grape.
+
+-- This is a test list for the `containsGrape` function with an grape that is not in the list.
+grapeList = [larrosaRose,castiglioni,lePetitHaitLafitte]
+newGrape = "Pinot Noir"
