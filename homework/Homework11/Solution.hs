63a057ba81743f126ee6619ddb90a315f5d61c65
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-11-29 16:48:25 +0100

    Updating solution for homework 11.

--- homework/Homework11/Solution.hs
+++ homework/Homework11/Solution.hs
@@ -1,6 +1,6 @@
 
-import System.Directory ( listDirectory )
-import Data.Time.Clock ( diffUTCTime, getCurrentTime )
+import System.Directory (listDirectory, doesFileExist)
+import Data.Time.Clock (diffUTCTime, getCurrentTime)
 
 -- Question 1
 -- Define an IO action that count the amount of files in the current directory 
@@ -37,3 +37,72 @@ createMsg = do
     putStrLn "Wrote message to msg.txt file."
     fileContent <- readFile "./msg.txt"
     putStrLn $ "File content is: " ++ fileContent
+
+{- Question 4
+Write a program that prints the directory tree structure from the current
+folder. Below you can see an example output how such a structure looks like.  
+.
+├── foo1.hs
+├── foo2.hs
+├── bar1
+    ├── bar2
+    ├── foo3.hs
+    ├── foo4.hs
+    └── bar3
+        └── foo5.hs
+└── bar5
+    ├── bar6
+    ├── foo6.hs
+    └── foo7.hs
+
+HINT: From the System.Directory module you can use the function doesFileExist
+which takes in a file path (String) and returns True if the item is a file. 
+-}
+
+data DirectoryStructure = File String | Folder String [IO DirectoryStructure]
+
+printDirectoryTree :: IO ()
+printDirectoryTree = do 
+    putStrLn "."
+    structure <- returnStructure "."
+    printStructure structure 0
+
+returnStructure :: FilePath -> IO [IO DirectoryStructure]
+returnStructure filePath = do
+    contents <- listDirectory filePath
+    if null contents 
+    then return []
+    else return $ flip map contents $ \fileName -> do
+                        let newFilePath = filePath ++ "/" ++ fileName
+                        isFile <- doesFileExist newFilePath
+                        if isFile
+                        then return $ File fileName
+                        else do 
+                            structure <- returnStructure newFilePath
+                            return $ Folder fileName structure  
+
+printStructure :: [IO DirectoryStructure] -> Int -> IO ()
+printStructure [] _ = return ()
+printStructure (x:xs) level = do
+    structure <- x
+    case structure of
+        File name -> do
+            printSpaces level
+            printElement xs name
+        Folder name ioList -> do
+            printSpaces level
+            printElement xs name
+            printStructure ioList (level + 1)
+    printStructure xs level
+
+printElement :: [IO DirectoryStructure] -> String -> IO ()
+printElement xs name = if null xs
+                       then putStrLn $ "└── " ++ name
+                       else putStrLn $ "├── " ++ name
+
+printSpaces :: Int -> IO ()
+printSpaces n = if n < 1 
+                then return ()
+                else do
+                    putStr "    "
+                    printSpaces (n - 1)
