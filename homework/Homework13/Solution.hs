6fab659fbee99fc95d3056c4156fe93bfe70edf3
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-11-30 13:14:34 +0100

    Adding to solution for homework 13.

--- homework/Homework13/Solution.hs
+++ homework/Homework13/Solution.hs
@@ -1,10 +1,35 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.ByteString as BS ( ByteString, pack, unpack )
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+import Data.ByteString as BS ( ByteString, pack, unpack, putStrLn, length )
 import Data.ByteString.Char8 as BC ( unpack )
 
 {-
+Question 1
+Write a function that takes in some text and a word and returns the number of words
+and lines the input text contains and the number of occurences of the input word. 
+-}
+
+textStatistics :: T.Text -> T.Text -> IO ()
+textStatistics text word = do
+    Prelude.putStrLn $ "The text contains " ++ show wordCount ++ " words."
+    Prelude.putStrLn $ "The text contains " ++ show lineCount ++ " lines."
+    TIO.putStrLn $ T.intercalate  "" ["The word ", word, " appears ", T.pack $ show occurances, " times."]
+  where wordCount = Prelude.length $ T.words text
+        lineCount = Prelude.length $ T.lines text
+        occurances = Prelude.length (T.splitOn word text) - 1
+
+sampleText :: T.Text
+sampleText = "This is some text.\n The text goes accross multiple lines."
+
+test1 :: IO ()
+test1 = textStatistics sampleText "text"
+
+{-
+Question 2
 Create a function that converts the binary representation of a ByteString into an Integer using a base 256 conversion.
 
 Example: we can convert [4,255,50] in base 256 to base 10 as 
@@ -21,12 +46,13 @@ byteStringToInteger b = let wrds = BS.unpack b
                             go [] = 0
                             go xs = toInteger(xs !! (Prelude.length xs - 1)) + 256 * go (Prelude.init xs)
 
-main1 :: IO ()
-main1 = do
+test2 :: IO ()
+test2 = do
     let hello = "Hello World!" :: BS.ByteString
     print $ byteStringToInteger hello
 
 {-
+Question 3
 Create a function that converts Integers into the binary representation using a base 256 conversion.
 For simplicity we will not represent the parity of the integers (the negative integers). 
 You can use the abs function to make all inputs positive.
@@ -44,8 +70,8 @@ integerToByteString n = BS.pack $ go (abs n)
                             go 0 = []
                             go m = go (m `div` 256) ++ [(toEnum . fromIntegral) (m `mod` 256)] 
 
-main2 :: IO ()
-main2 = do
+test3 :: IO ()
+test3 = do
     let hello = "Hello World!" :: BS.ByteString
         integerHello = byteStringToInteger hello
     print $ BC.unpack $ integerToByteString integerHello
