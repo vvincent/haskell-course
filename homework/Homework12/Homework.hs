e4c089775d17f557cbd704c00ee369f4795039de
Luka Kurnjek <luka.kurnjek@iohk.io>
2022-12-16 14:34:08 +0100

    Updating homework for lesson 12.

--- homework/Homework12/Homework.hs
+++ homework/Homework12/Homework.hs
@@ -1,9 +1,4 @@
 
-import Data.Array
-import Data.Array.Unboxed
-import Data.Time (getCurrentTime)
-import Data.Time.Clock (diffUTCTime)
-
 -- Question 1
 -- Write a function that takes in an integer n and returns a list of the 
 -- first n fibonacci numbers. Use the function scanl to achieve this.
@@ -16,32 +11,9 @@ test1 :: IO ()
 test1 = print $ fibs 7
 
 -- Question 2
--- In homework for lesson 11 you had to write a program that times an IO action.
--- Create an Array and UArray variables that have both 100.000 elements and time 
--- how long does it take to access the last element. 
-
-timeIO :: IO a -> IO ()
-timeIO io = do 
-    initTime <- getCurrentTime
-    result <- io
-    finalTime <- getCurrentTime
-    let diff = diffUTCTime finalTime initTime
-    putStrLn $ "Time used for io action is: " ++ show diff
-
-printArrayElem :: IO ()
-printArrayElem = do
-    let array1 = Data.Array.listArray (1,100000) [1..100000]
-    print $ array1 Data.Array.! 100000
-
-printUArrayElem :: IO ()
-printUArrayElem = do
-    let array1 = Data.Array.Unboxed.array (1,100000) $ zip [1..100000] [1..100000] :: UArray Int Int
-    print $ array1 Data.Array.Unboxed.! 100000
-
-test2 :: IO ()
-test2 = do
-    timeIO printArrayElem
-    timeIO printUArrayElem
+-- TODO .. For instance create a cabal project that uses the function splitOn from
+-- the module Data.List.Split. Because the package split does not come with the 
+-- default Haskell installation it needs to be added to the cabal file as dependency.
 
 -- Question 3
 -- Create a cabal project with an app/ folder that contains Main.hs and a lib/
