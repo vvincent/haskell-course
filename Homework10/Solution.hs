3b44d85015ab00274c4b10f81eca7aa54f256a09
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2022-12-20 17:22:42 +0000

    finished homework lesson 10

--- Homework10/Solution.hs
+++ Homework10/Solution.hs
@@ -1,73 +1,151 @@
+{-
+-- Question 1 --
+Continuing with the logistics software of the lesson:
+ 1. After using the `Container` type class for a while, you realize that it might need a few adjustments:
+  	- First, write down the `Container` type class and its instances, same as we did in the lesson
+  	  (try to do it without looking and check at the end or if you get stuck).
+  	- Then, add a function called `unwrap` that gives you back the value inside a container.
+ 2. Create an instance for the `MailedBox` data type.
+ 	- The MailedBox data type represents a box sent through the mail.
+ 	- The parameter `t` is a tag with a person's identifier
+ 	- The parameter `d` is the person's details (address,etc).
+ 	- The parameter `a` is the content of the MailedBox
+-}
 
--- Question 1
--- Below you have a data type defined that holds a persons information. If you are deriving 
--- the Ord class and use the sort function on the unsortedPersons list the persons are sorted 
--- by their name from smallest to largest first letter. 
--- Implement the Ord type class for the PersonData type such that sort function will sort the 
--- persons regarding to their age from largest to smallest.
+data MailedBox t d a = EmptyMailBox t d | MailBoxTo t d a
 
-import Data.List (sort)
+data Box a = Empty | Has a
 
-type Name = String
-type Age = Int
-type Height = Int
+data Present t a = EmptyPresent t | PresentFor t a
 
-newtype PersonData = PersonData (Name, Age, Height) deriving (Show, Eq)
+-- Container type class
+class Container c where
+  isEmpty :: c a -> Bool
+  contains :: (Eq a) => c a -> a -> Bool
+  replace :: c a -> b -> c b
+  unwrap :: c a -> a -> a
 
-unsortedPersons :: [PersonData]
-unsortedPersons = [ PersonData ("Mark", 65, 173)
-                  , PersonData ("Jimmy", 45, 182)
-                  , PersonData ("Brian", 55, 178)]
+-- Container instance for Box type
+instance Container Box where
+  isEmpty Empty = True
+  isEmpty _ = False
 
--- sort unsortedPersons -- when deriving the Ord type class for PersonData type
--- [PersonData ("Brian",55,178),PersonData ("Jimmy",45,182),PersonData ("Mark",65,173)]
+  contains Empty _ = False
+  contains (Has x) y = x == y
 
-instance Ord PersonData where
-  compare (PersonData (name1, age1, height1)) (PersonData (name2, age2, height2)) = compare age2 age1
+  replace _ = Has -- Point-free version of: replace _ x = Has x
 
+  unwrap Empty x = x
+  unwrap (Has x) _ = x
 
--- Question 2
--- Create the type "Position" that can have the values: Intern, Junior, Senior, Manager, Chief.
--- Then create the type Experience that can have the values: Programming, Managing, Leading.
--- Create a function that takes in two candidates that have a Experience value and years of experience 
--- provided as an integer. And the function should returs the position apropriate for the candidate
--- and also say which candidate has priority for employment (The higher Position gives higher
--- priority and for same positions the years of experience can be compared). Test the function on a
--- set of three candidates that have experience and years: Programming 7, Programming 8, Managing 5.
+-- Container instance for (Present t) type
+instance Container (Present t) where
+  isEmpty (EmptyPresent _) = True
+  isEmpty _ = False
 
+  contains (EmptyPresent _) _ = False
+  contains (PresentFor _ x) y = x == y
+
+  replace (EmptyPresent t) x = PresentFor t x
+  replace (PresentFor t _) x = PresentFor t x
+
+  unwrap (EmptyPresent _) x = x
+  unwrap (PresentFor _ x) _ = x
+
+-- Container instance for (MailedBox t d) type
+instance Container (MailedBox t d) where
+  isEmpty (EmptyMailBox _ _) = True
+  isEmpty _ = False
+
+  contains (EmptyMailBox _ _) _ = False
+  contains (MailBoxTo _ _ x) y = x == y
+
+  replace (EmptyMailBox t d) x = MailBoxTo t d x
+  replace (MailBoxTo t d _) x = MailBoxTo t d x
+
+  unwrap (EmptyMailBox _ _) x = x
+  unwrap (MailBoxTo _ _ x) _ = x
+
+-- Question 2 --
+-- Create instances of Show, Eq, and Ord for these three data types (use
+-- automatic deriving whenever possible):
+
+{-
+We can safely derive Show virtually always.
+We can safely derive Eq for Position because the value constructors have no parameters.
+We can safely derive Ord for Position because the value constructors have no parameters.
+-}
 data Position = Intern | Junior | Senior | Manager | Chief deriving (Show, Eq, Ord)
+
+{-
+We can safely derive Show virtually always.
+We can safely derive Eq for Experience because the value constructors have no parameters.
+We can safely derive Ord for Experience because the value constructors have no parameters.
+-}
 data Experience = Programming | Managing | Leading deriving (Show, Eq, Ord)
 
-type WorkingYears = Int
-data Candidate = CandidateData Experience WorkingYears deriving (Show, Eq, Ord)
+-- No deriving here. Address is just another name for String
+type Address = String
 
-candidate1 :: Candidate
-candidate1 = CandidateData Programming 7
+{-
+We can safely derive Show virtually always.
+We shouldn't derive Eq because there is a relationship between USD, EUR, and ADA.
+We shouldn't derive Ord because there is a relationship between USD, EUR, and ADA.
+-}
+data Salary = USD Double | EUR Double deriving (Show)
 
-candidate2 :: Candidate
-candidate2 = CandidateData Programming 8
+{-
+Helper value to make it easier to define the instances.
+Here, we set a fixed value, but in a real app, we would use an API to continuously
+get an updated value. We'll learn how to do this in future lessons.
+-}
+rateUsdEur = 1.1
 
-candidate3 :: Candidate
-candidate3 = CandidateData Managing 5
+instance Eq Salary where
+  USD x == USD y = x == y
+  EUR x == EUR y = x == y
+  USD x == EUR y = x * rateUsdEur == y
+  EUR x == USD y = x == y * rateUsdEur
 
-data Assesment = Assesment Position Position String deriving Show
-compareCandidates :: Candidate -> Candidate -> Assesment
-compareCandidates c1 c2 = Assesment p1 p2 msg 
-  where p1 = assesCandidate c1
-        p2 = assesCandidate c2
-        msg
-          | c1 < c2 = "Second candidate has priority."
-          | c1 > c2 = "First candidate has priority."
-          | otherwise = "Candidates have same priority"
+instance Ord Salary where
+  USD x `compare` USD y = compare x y
+  EUR x `compare` EUR y = compare x y
+  USD x `compare` EUR y = compare (x * rateUsdEur) y
+  EUR x `compare` USD y = compare x (y * rateUsdEur)
 
-assesCandidate :: Candidate -> Position
-assesCandidate (CandidateData ex wy)
-    | ex == Programming = getProgramingLevel wy
-    | ex == Managing = Manager
-    | ex == Leading = Chief
+{-
+We can safely derive Show virtually always.
+We can safely derive Eq for Relationship because the value constructors don't have an
+implicit relationship.
+We can safely derive Ord for Relationship because the value constructors don't have an
+implicit relationship.
+-}
+data Relationship
+  = Contractor Position Experience Salary Address
+  | Employee Position Experience Salary Address
+  deriving (Show, Eq, Ord)
 
-getProgramingLevel :: Int -> Position
-getProgramingLevel wy
-    | wy < 2 = Intern
-    | wy < 6 = Junior
-    | otherwise = Senior
+{-
+- We can safely derive Show virtually always.
+- We can safely derive Eq for Pokemon. We could implement a more efficient Eq instance
+  that only compares the pPokedexNum or pName (since that's unique for a Pokemon). But
+  there's no need since two values of the same pokemon should have all fields equal.
+- We shouldn't derive Ord because it would compare all the fields lexicographically. In
+  that case, "Charizard" (#6) would come before "Venusaur" (#3). Because the name is
+  the first field to be compared, and "C" comes before "V." We both know that's unacceptable,
+  so we have to compare only the PokeDex number.
+-}
+data Pokemon = Pokemon
+  { pName :: String,
+    pType :: [String],
+    pGeneration :: Int,
+    pPokeDexNum :: Int
+  }
+  deriving (Show, Eq)
+
+instance Ord Pokemon where
+  compare Pokemon {pPokeDexNum = p1} Pokemon {pPokeDexNum = p2} = compare p1 p2
+
+charizard = Pokemon "Charizard" ["Fire", "Flying"] 1 6
+
+venusaur = Pokemon "Venusaur" ["Grass", "Poison"] 1 3
