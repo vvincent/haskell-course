86bfa4d0b8514671b427279ef450b5ec5a115cc6
Robertino <48034748+rober-m@users.noreply.github.com>
2023-01-24 13:16:35 +0000

    Update Homework.hs

--- Homework/Homework11/Homework.hs
+++ Homework/Homework11/Homework.hs
@@ -1,69 +1,91 @@
-
-import System.Directory (listDirectory, doesFileExist)
-import System.CPUTime (getCPUTime)
 import Data.List
+import System.CPUTime (getCPUTime)
+import System.Directory (doesFileExist, listDirectory)
+import Text.XHtml (thead)
 
--- Question 1
--- Define an IO action that count the number of files in the current directory 
--- and prints it to the terminal.
+{-
+We imported some functions that you'll need to complete the homework.
+FilePath is just a synonym for String. Although, make sure to follow the standard path
+representation when using them (https://en.wikipedia.org/wiki/Path_(computing).
+getCPUTime    :: IO Integer
+doesFileExist :: FilePath -> IO Bool
+listDirectory :: FilePath -> IO [FilePath]
+You can hover over the functions to know what they do.
+-}
 
-listFiles :: IO ()
+{-
+-- Question 1 --
+Define an IO action that counts the number of files in the current directory
+and prints it to the terminal inside a string message.
+-}
 
--- In cryptography prime numbers, positive integers only divisible by itself and 1, play a fundamental role as 
--- they provide unbreakable mathematical structures. These structures are leverage to establish secure
--- cryptographic primitives like encryption!
---
--- But, generating primes is a computational straining problem, as we will measure in the following exercise.
--- This is because, to know whether a number is a prime number, you first need to know all the previous
--- primes, and then check that they are not a divisor of this number. So, this problem gets 
--- bigger and bigger! The goal of this exercise is to showcase different functional algorithms and their performance.
---
--- Below are three functions (primes1, primes2 and primes3) which all generate a list of prime numbers.
+-- listFiles :: IO ()
+
+{-
+-- Question 2 --
+Write an IO action that asks the user to type something, then writes the message
+to a file called msg.txt, and after that, it reads the text from the msg.txt
+file and prints it back. Use the writeFile and readFile functions.
+-}
+
+-- createMsg :: IO ()
+
+
+{-
+-- Context for Questions 3 and 4 --
+In cryptography, prime numbers (positive integers only divisible by themselves and 1) play a fundamental
+role in providing unbreakable mathematical structures. These structures, in turn, are leveraged to
+establish secure encryption.
+But, generating primes is a computational straining problem, as we will measure in the following exercise.
+This is because, to know whether a number is a prime number, you first need to know all the previous primes
+and then check that they are not a divisor of this number. So, this problem gets bigger and bigger!
+Our lead cryptographer provided us with 3 different algorithms (primes1, primes2, and primes3). All three
+correctly produce a list of all the prime numbers until a limit (that we provide as a parameter).
+Our job is not to understand these algorithms but to measure which is the fastest and print the largest
+prime number below our limit. Do it step by step, starting with question 3.
+-}
 
 primes1 :: Integer -> [Integer]
-primes1 m = sieve [2..m]
-  where
-    sieve [] = []
-    sieve (p:xs) = p : sieve [x|x <- xs, x `mod` p > 0]
+primes1 m = sieve [2 .. m]
+ where
+  sieve [] = []
+  sieve (p : xs) = p : sieve [x | x <- xs, x `mod` p > 0]
 
 primes2 :: Integer -> [Integer]
-primes2 m = sieve [2..m]
-             where 
-             sieve (x:xs) = x : sieve (xs \\ [x,x+x..m])
-             sieve [] = []
+primes2 m = sieve [2 .. m]
+ where
+  sieve (x : xs) = x : sieve (xs \\ [x, x + x .. m])
+  sieve [] = []
 
 primes3 :: Integer -> [Integer]
-primes3 m = turner [2..m] 
-    where
-    turner []     = []
-    turner (p:xs) = p : turner [x | x<-xs, x<p*p || rem x p /= 0]
+primes3 m = turner [2 .. m]
+ where
+  turner [] = []
+  turner (p : xs) = p : turner [x | x <- xs, x < p * p || rem x p /= 0]
 
--- Question 2
--- Define an IO action that calculates the time another IO action takes.
--- Use the getCPUTime :: IO Integer function to get the CPU time before and after the IO action.
--- The CPU time here is given in picoseconds (which 1/1000000000000th of a second).
+{-
+-- Question 3 --
+Define an IO action that takes an IO action as input and calculates the time it takes to execute.
+Use the getCPUTime :: IO Integer function to get the CPU time before and after the IO action.
+The CPU time here is given in picoseconds (which is 1/1000000000000th of a second).
+-}
 
-timeIO :: IO a -> IO ()
+-- timeIO :: IO a -> IO ()
 
--- Question 3
--- Write an `IO ()` action that retrieves an integer from the user via getLine.
--- Use read to parse this input and with it time the execution of retrieving the 
--- last element of the primes in `[2..m]`. 
 
-benchmark :: IO ()
+{-
+-- Question 4 --
+Write an action that retrieves a value from the standard input, parses it as an integer,
+and compares the time all three algorithms take to produce the largest prime before the
+limit. Print the number and time to the standard output.
+-}
 
--- Question 4
--- Write an IO action that asks the user to type something and the program then 
--- writes the message to a file called msg.txt. After that it reads the text from
--- the msg.txt file and prints it back. Use the writeFile and readFile functions.
+-- benchmark :: IO ()
 
-createMsg :: IO ()
-
-{- Question 5 
-
-Advanced example for students aiming at Plutus.
-Write a program that prints the directory tree structure from the current
-folder (denoted by "."). Below you can see an example output how such a structure looks like.  
+{-
+ -- Question 5 -- EXTRA CREDITS -- (In case the previous ones were too easy)
+Write a program that prints the directory tree structure from the current folder.
+Below you can see an example output of how such a structure looks like:
 .
 ├── foo1.hs
 ├── foo2.hs
@@ -77,6 +99,6 @@ folder (denoted by "."). Below you can see an example output how such a structur
     ├── bar6
     ├── foo6.hs
     └── foo7.hs
-HINT: From the System.Directory module you can use the function doesFileExist
-which takes in a file path (String) and returns True if the item is a file. 
+HINT: You can use the function doesFileExist, which takes in a FilePath and returns
+True if the argument file exists and is not a directory, and False otherwise.
 -}
