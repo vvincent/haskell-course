a92f215fe11afc256dec6e3273c49a97079db815
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2024-05-06 14:05:46 +0000

    finished homework 19

--- Homework19/Homework19.hs
+++ Homework19/Homework19.hs
@@ -28,5 +28,26 @@ result = (\x y z -> x * y + z) <$> (3 `eDiv` 2) <*> Right 2 <*> pure 4
 ----------------------------------------------------------------------------------------------------
 ---------------------------------------- Question 2 ------------------------------------------------
 {-
-TODO
+ - Implement all these Applicative functions
 -}
+
+-- Conditional execution of 'Applicative' expressions. For example,
+-- > when debug (putStrLn "Debugging")
+-- will output the string @Debugging@ if the Boolean value @debug@
+-- is 'True', and otherwise do nothing.
+when :: (Applicative f) => Bool -> f () -> f ()
+when p s  = if p then s else pure ()
+
+-- The reverse of 'when'.
+unless            :: (Applicative f) => Bool -> f () -> f ()
+unless p s        =  if p then pure () else s
+
+-- Like 'replicateM', but discards the result.
+replicateM_ :: (Applicative m) => Int -> m a -> m ()
+replicateM_ n action | n <= 0    = pure ()
+                     | otherwise = action *> replicateM_ (n-1) action
+
+
+--------------------------------------------------------------------------------------------------------
+-- NEXT LESSON IS GOING TO BE ABOUT USING APPLICATIVE ON A REAL WORLD PROBLEM. SO, WE'LL CUT IT HERE.
+--------------------------------------------------------------------------------------------------------
